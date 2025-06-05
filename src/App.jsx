bd5f2a391ac52f0a8b7a4d4e28663585bbf67097
Valeria Kartasheva <valeriakartaseva39@gmail.com>
2025-06-05 15:06:03 +0300

    commit без JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,44 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 export const App = () => {
 	// Императивный стиль
-    const date = new Date().getFullYear()
-	 // Декларативный стиль
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>
-					{date}
-				</p>
-			</header>
-		</div>
+	const date = new Date().getFullYear();
+	// Декларативный стиль
+	return createElement(
+		'div',
+		{
+			className: 'App',
+		},
+		createElement(
+			'header',
+			{
+				className: 'App-header',
+			},
+			createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('p', null, date),
+		),
 	);
-
-}
-
+};
