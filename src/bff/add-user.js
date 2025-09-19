428fc342acbc5e49fd2450a15c136e13cdcb4927
Valeria Kartasheva <valeriakartaseva39@gmail.com>
2025-09-19 23:19:55 +0300

    Создали BFF

---
+++ src/bff/add-user.js
@@ -0,0 +1,15 @@
+export const addUser = (login, password) => {
+	fetch('http://localhost:3005/users', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json;charset=utf-8',
+		},
+		body: JSON.stringify({
+			// id: '001',
+			login,
+			password,
+			registed_at: new Date().toISOString().replace('T', '-').substring(0, 10),
+			role_id: 2,
+		}),
+	});
+};
