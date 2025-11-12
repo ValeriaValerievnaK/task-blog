8e1b69751e2d4273f442afd57861b636c93f4a49
Valeria Kartasheva <valeriakartaseva39@gmail.com>
2025-11-12 20:36:53 +0300

    chores

--- backend/helpers/token.js
+++ backend/helpers/token.js
@@ -1,13 +1,13 @@
 const jwt = require("jsonwebtoken");
 
-const sing = process.env.JWT_SECRET;
+const sign = process.env.JWT_SECRET;
 
 module.exports = {
   generate(data) {
-    return jwt.sign(data, sing, { expiresIn: "30d" });
+    return jwt.sign(data, sign, { expiresIn: "30d" });
   },
 
   verify(token) {
-    return jwt.verify(token, sing);
+    return jwt.verify(token, sign);
   },
 };
